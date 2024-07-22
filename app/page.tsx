e07f1f08135a14ae1dd5a0f8e420696a3d22f363
xh3b4sd <xh3b4sd@gmail.com>
2024-07-22 19:26:49 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import { InfoCircleIcon } from "@/components/icon/base/InfoCircleIcon";
 import { ErrorToast, InfoToast, SuccessToast, ToastRoot } from "@/components/toast/InfoToast";
 
 export default function Home() {
-  const a = ErrorToast();
-  const b = InfoToast();
-  const c = SuccessToast();
+  const err = ErrorToast();
+  const inf = InfoToast();
+  const suc = SuccessToast();
 
   return (
     <Toast.Provider duration={10 * 1000} swipeDirection="right">
@@ -21,26 +21,26 @@ export default function Home() {
           <ThemeSwitch />
 
           <button onClick={() => {
-            a.new("something's wrong");
+            err.new("something's wrong");
           }}>
             <InfoCircleIcon />
           </button>
 
           <button onClick={() => {
-            b.new("you should read this");
+            inf.new("you should read this");
           }}>
             <InfoCircleIcon />
           </button>
 
           <button onClick={() => {
-            c.new("this is great");
+            suc.new("this is great");
           }}>
             <InfoCircleIcon />
           </button>
 
-          <ToastRoot ref={a.ref()} />
-          <ToastRoot ref={b.ref()} />
-          <ToastRoot ref={c.ref()} />
+          <ToastRoot ref={err.ref()} />
+          <ToastRoot ref={inf.ref()} />
+          <ToastRoot ref={suc.ref()} />
 
           <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
         </div>
--- components/toast/InfoToast.tsx
+++ components/toast/InfoToast.tsx
@@ -20,6 +20,17 @@ interface ToastRootState {
   size: number;
 }
 
+// ToastRoot needs to be mounted in some react component using the reference of
+// a toast object.
+//
+//     const err = ErrorToast();
+//     <ToastRoot ref={err.ref()} />
+//
+// Once the ToastRoot component is setup, the toast object can be used in the
+// local scope to emit a toast message.
+//
+//     err.new("something's wrong");
+//
 export class ToastRoot extends React.Component<{}, ToastRootState> {
   constructor(props: {}) {
     super(props);
@@ -75,13 +86,17 @@ export class ToastRoot extends React.Component<{}, ToastRootState> {
   }
 };
 
+// ToastBody is a react component that we only use to wrap some super annoying
+// workaround to make the currently used toast design work. The wrapped react
+// component is ToastRoot, which is used in any given local scope in which toast
+// messages are used to provide feedback about site interactions.
 export class ToastBody extends React.Component<ToastBodyProps> {
   private reference: React.RefObject<ToastRoot>;
 
   constructor(props: ToastBodyProps) {
     super(props);
 
-    this.reference = React.useRef<ToastRoot>(new ToastRoot({}));
+    this.reference = React.createRef<ToastRoot>();
   }
 
   ref(): React.RefObject<ToastRoot> {
