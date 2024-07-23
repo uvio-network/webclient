dfae7defd680239deb409404799ad934e80233ee
xh3b4sd <xh3b4sd@gmail.com>
2024-07-23 14:32:13 +0200

    fix

--- components/button/ToastButton.tsx
+++ components/button/ToastButton.tsx
@@ -6,6 +6,12 @@ interface Props {
   onClick?: () => void;
 }
 
+// ToastButton is primarily used for the close action in the toast component. We
+// need a separate "button" using only a div element there, because we cannot
+// use button elements within button elements. In other words, because the Radix
+// component uses a button element for the close action already, our "button"
+// component here cannot use a button element again, which is why we use a div
+// element to wrap the child components.
 export const ToastButton = (props: Props) => {
   return (
     <div
