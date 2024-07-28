926da195dbdd70de55a6efa611bbbf2ab8517472
xh3b4sd <xh3b4sd@gmail.com>
2024-07-28 16:31:57 +0200

    fix

--- components/auth/AuthPage.tsx
+++ components/auth/AuthPage.tsx
@@ -1,16 +1,16 @@
-"use client";
-
-import * as Privy from "@privy-io/react-auth";
-import * as React from "react";
-
 // TODO
-export const AuthPage = (Component: React.ComponentType) => {
-  return (props: any) => {
-    const { authenticated, login, ready } = Privy.usePrivy();
+// "use client";
 
-    if (!ready) return <></>;
-    if (!authenticated) return login();
+// import * as Privy from "@privy-io/react-auth";
+// import * as React from "react";
 
-    return <Component {...props} />;
-  };
-};
+// export const AuthPage = (Component: React.ComponentType) => {
+//   return (props: any) => {
+//     const { authenticated, login, ready } = Privy.usePrivy();
+
+//     if (!ready) return <></>;
+//     if (!authenticated) return login();
+
+//     return <Component {...props} />;
+//   };
+// };
