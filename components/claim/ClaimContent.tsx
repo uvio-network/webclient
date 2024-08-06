732e44472fcaa31b8196545d4f18d6da07f57122
xh3b4sd <xh3b4sd@gmail.com>
2024-08-06 13:39:41 +0200

    redirect to claim page on markdown click (#32)

--- components/claim/ClaimContent.tsx
+++ components/claim/ClaimContent.tsx
@@ -1,13 +1,26 @@
+import * as React from "react";
+
 import { ClaimObject } from "@/modules/claim/object/ClaimObject";
 import { RenderMarkdown } from "@/components/markdown/RenderMarkdown";
+import { useRouter } from "next/navigation";
 
 interface Props {
   claim: ClaimObject;
 }
 
 export const ClaimContent = (props: Props) => {
+  const router = useRouter();
+
+  const onClick = (e: React.MouseEvent) => {
+    const t = e.target as HTMLElement;
+
+    if (t.tagName === "A") return;
+
+    router.push("/claim/" + props.claim.id());
+  };
+
   return (
-    <div className="">
+    <div className="" onClick={onClick}>
       <RenderMarkdown
         text={props.claim.markdown()}
       />
--- components/claim/ClaimHeader.tsx
+++ components/claim/ClaimHeader.tsx
@@ -1,23 +1,34 @@
+import Link from "next/link";
+
 import { ClaimHeaderMenu } from "@/components/claim/ClaimHeaderMenu";
 import { ClaimObject } from "@/modules/claim/object/ClaimObject";
 import { StarLineIcon } from "@/components/icon/StarLineIcon";
-import Link from "next/link";
 
 interface Props {
   claim: ClaimObject;
 }
 
 export const ClaimHeader = (props: Props) => {
+  const image = props.claim.owner().image();
+
   return (
     <div className="flex">
-      <div className="flex-none w-12 h-12 mr-2 rounded background-overlay border border-color overflow-hidden">
-        <Link href="/">
-          <img src={props.claim.owner().image()} />
-        </Link>
+      <div className={`
+        flex-none w-12 h-12 mr-2 rounded overflow-hidden
+        ${image === "" && "background-overlay border border-color"}
+      `}
+      >
+        {image !== "" && (
+          <Link href={"/user/" + props.claim.owner().id()}>
+            <img src={image} />
+          </Link>
+        )}
       </div>
       <div className="flex-1 w-full">
         <div className="font-medium">
-          {props.claim.owner().name()}
+          <Link href={"/user/" + props.claim.owner().id()}>
+            {props.claim.owner().name()}
+          </Link>
         </div>
         <div className="flex h-6">
           <div className="my-auto text-gray-500 dark:text-gray-400 text-sm">
