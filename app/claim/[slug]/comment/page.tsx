253d91683647d58f4e8f2bc3e11025524ba8d910
xh3b4sd <xh3b4sd@gmail.com>
2024-08-08 12:03:53 +0200

    fix

--- app/claim/[slug]/comment/page.tsx
+++ app/claim/[slug]/comment/page.tsx
@@ -2,17 +2,38 @@
 
 import * as React from "react";
 
+import { ClaimObject } from "@/modules/claim/object/ClaimObject";
 import { ClaimPreview } from "@/components/app/claim/comment/preview/ClaimPreview";
 import { EditorButton } from "@/components/app/claim/comment/editor/EditorButton";
 import { EditorStore } from "@/components/app/claim/comment/editor/EditorStore";
+import { EmptyUserSearchResponse } from "@/modules/api/user/search/Response";
 import { MarkdownField } from "@/components/app/claim/comment/field/MarkdownField";
 import { MarkdownPreview } from "@/components/app/claim/comment/preview/MarkdownPreview";
+import { PostSearch } from "@/modules/api/post/search/Search";
 import { SubmitButton } from "@/components/app/claim/comment/editor/SubmitButton";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Page({ params }: { params: { slug: string } }) {
   const [edit, setEdit] = React.useState<boolean>(true);
 
   const editor = EditorStore.getState();
+
+  const claim = useQuery({
+    queryKey: ["claim", params.slug, "comment", "PostSearch"],
+    queryFn: async () => {
+      const pos = await PostSearch("", [{ id: params.slug }]);
+
+      for (const x of pos) {
+        if (x.id === params.slug) {
+          if (x.kind === "claim") {
+            return new ClaimObject(x, EmptyUserSearchResponse(), undefined, []);
+          } else {
+            return undefined;
+          }
+        }
+      }
+    },
+  })
 
   React.useEffect(() => {
     editor.updateClaim(params.slug)
@@ -20,34 +41,43 @@ export default function Page({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <div className="flex mb-6 w-full items-center">
-        <EditorButton
-          active={edit}
-          onClick={() => setEdit((old) => !old)}
-          text="Write"
-        />
-
-        <EditorButton
-          active={!edit}
-          onClick={() => setEdit((old) => !old)}
-          text="Preview"
-        />
-      </div>
-
-      {edit ? (
-        <MarkdownField />
-      ) : (
-        <MarkdownPreview />
+      {!claim.data && (
+        <>
+          claim not found
+        </>
       )}
 
-      <ClaimPreview
-        query={["claim", params.slug, "comment"]}
-        request={[{ id: params.slug }]}
-      />
+      {claim.data && (
+        <>
+          <div className="flex mb-6 w-full items-center">
+            <EditorButton
+              active={edit}
+              onClick={() => setEdit((old) => !old)}
+              text="Write"
+            />
 
-      <div className="grid place-content-end">
-        <SubmitButton />
-      </div>
+            <EditorButton
+              active={!edit}
+              onClick={() => setEdit((old) => !old)}
+              text="Preview"
+            />
+          </div>
+
+          {edit ? (
+            <MarkdownField />
+          ) : (
+            <MarkdownPreview />
+          )}
+
+          <ClaimPreview
+            claim={claim.data}
+          />
+
+          <div className="grid place-content-end">
+            <SubmitButton />
+          </div>
+        </>
+      )}
     </>
   );
 };
