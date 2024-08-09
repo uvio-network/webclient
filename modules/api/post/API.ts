9a20e92b507692b84a254c7deda186eb45662b72
xh3b4sd <xh3b4sd@gmail.com>
2024-08-09 15:42:01 +0200

    add network configuration (#40)

--- modules/api/post/API.ts
+++ modules/api/post/API.ts
@@ -4,6 +4,6 @@ import { TwirpFetchTransport } from "@protobuf-ts/twirp-transport";
 import { APIClient } from "@uvio-network/apitscode/src/post/api.client";
 
 export const API = new APIClient(new TwirpFetchTransport({
-  baseUrl: Config.ApiServerRPCEndpoint,
-  sendJson: Config.ApiServerRPCSendJSON,
+  baseUrl: Config.ApiServerRpcEndpoint,
+  sendJson: Config.ApiServerRpcSendJSON,
 }));
---
+++ modules/biconomy/BundlerURL.ts
@@ -0,0 +1,10 @@
+// BundlerURL returns the formatted bundler URL for the Biconomy provider. Note
+// that at the moment the bundler's API key is globally provided by Biconomy.
+//
+//     inp[0] the chain ID where the bundler is being used
+//     inp[1] the Biconomy API key for the environment being used
+//     out[1] the formatted bundler URL for the Biconomy provider
+//
+export const BundlerURL = (cid: string, key: string = "nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44"): string => {
+  return `https://bundler.biconomy.io/api/v2/${cid}/${key}`;
+};
