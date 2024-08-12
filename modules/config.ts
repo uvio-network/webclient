d4df2aa141b84a0b2cb56f4c70bd0b0a9176ca56
xh3b4sd <xh3b4sd@gmail.com>
2024-08-12 16:20:03 +0200

    fix

--- modules/config.ts
+++ modules/config.ts
@@ -5,7 +5,7 @@ export const ArbitrumSepoliaAlchemyRpcEndpoint: string = process.env.NEXT_PUBLIC
 export const ArbitrumSepoliaBiconomyPaymasterApiKey: string = process.env.NEXT_PUBLIC_ARBITRUM_SEPOLIA_BICONOMY_PAYMASTER_API_KEY || ""
 export const ArbitrumSepoliaUvioMarketsContract: string = process.env.NEXT_PUBLIC_ARBITRUM_SEPOLIA_UVIO_MARKETS_CONTRACT || ""
 
-export const DefaultChainId: number = Number(process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID) || 0
+export const DefaultChainId: number = Number(process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID) || 421614
 
 export const PrivyAppId: string = process.env.NEXT_PUBLIC_PRIVY_APP_ID || "clz1hy6f302wbx56o75ry96nm"
 export const PrivyClientId: string = process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID || "client-WY2onuhfaV4hp8h313qp56xF4BiY6tF3bh9idyWCASLct"
