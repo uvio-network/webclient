e64b85e7bf07b9cd447733e2b7ffddb6511c69e7
xh3b4sd <xh3b4sd@gmail.com>
2024-08-15 00:37:06 +0200

    render user activity page

---
+++ app/user/[slug]/activity/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ClaimList } from "@/components/claim/ClaimList";
+import { PageHeader } from "@/components/page/PageHeader";
+
+export default function Page({ params }: { params: { slug: string } }) {
+  return (
+    <>
+      <PageHeader titl="User Activity" />
+      <ClaimList
+        query={["claim", "vote", params.slug]}
+        request={[{ vote: params.slug }]}
+      />
+    </>
+  );
+};
