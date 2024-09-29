cb2fca996d1d80c479297ee1def146293f73d5e6
xh3b4sd <xh3b4sd@gmail.com>
2024-09-29 22:08:27 +0200

    fix

--- app/claim/[slug]/page.tsx
+++ app/claim/[slug]/page.tsx
@@ -1,15 +1,14 @@
 import fs from "fs";
-import Head from "next/head";
 import satori from "satori";
 import sharp from "sharp";
 
 import { Client } from "@/app/claim/[slug]/client";
-import { PostSearch } from "@/modules/api/post/search/Search";
-import { NewClaimSummary } from "@/modules/claim/ClaimSummary";
 import { Metadata } from "next";
-import { LimitMarkdown } from "@/modules/string/LimitMarkdown";
-import { RenderMarkdown } from "@/components/markdown/RenderMarkdown";
-import { SplitList } from "@/modules/string/SplitList";
+import { NewClaimSummary } from "@/modules/claim/ClaimSummary";
+import { PostSearch } from "@/modules/api/post/search/Search";
+import React from "react";
+
+const font = fs.readFileSync("fonts/Inter-Regular.ttf");
 
 interface Props {
   params: { slug: string };
@@ -17,14 +16,53 @@ interface Props {
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const [pos] = await PostSearch("", [{ id: props.params.slug }]);
-  const sum = NewClaimSummary(pos);
 
-  const lab = SplitList(pos.labels);
+  const sum = NewClaimSummary(pos);
   const tot = sum.agreement + sum.disagreement;
   const tit = `${tot.toFixed(2)} ${pos.token} staked on Uvio, do you agree?`;
   const per = (sum.agreement / tot) * 100;
 
   const svg = await satori(
+    imgDiv(tit, per),
+    {
+      width: 764,
+      height: 400,
+      fonts: [
+        {
+          name: "Inter-Regular",
+          data: font,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    },
+  );
+
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+  const img = `data:image/png;base64,${png.toString("base64")}`;
+
+  return {
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": img,
+      "fc:frame:image:aspect_ratio": "1.91:1",
+      "fc:frame:button:1": "Stake",
+      "fc:frame:button:1:action": "link",
+      "fc:frame:button:1:target": process.env.__NEXT_PRIVATE_ORIGIN + "/claim/" + props.params.slug,
+      "og:title": tit,
+      "og:image": img,
+    },
+  };
+};
+
+export default function Page(props: Props) {
+  return (
+    <Client slug={props.params.slug} />
+  );
+};
+
+const imgDiv = (tit: string, per: number): React.ReactNode => {
+  return (
     <div style={{
       color: "white",
       backgroundImage: "linear-gradient(135deg, #17101f, #3b2f59, #734b91)",
@@ -67,41 +105,5 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
         % chance of being true
       </div>
     </div>
-    ,
-    {
-      width: 764,
-      height: 400,
-      fonts: [
-        {
-          name: "Inter-Regular",
-          data: fs.readFileSync("fonts/Inter-Regular.ttf"),
-          weight: 400,
-          style: "normal",
-        },
-      ],
-    },
   );
-
-  const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
-  const base64Image = pngBuffer.toString('base64');
-  const dataUri = `data:image/png;base64,${base64Image}`;
-
-  return {
-    other: {
-      "fc:frame": "vNext",
-      "fc:frame:image": dataUri,
-      "fc:frame:image:aspect_ratio": "1.91:1",
-      "fc:frame:button:1": "Stake",
-      "fc:frame:button:1:action": "link",
-      "fc:frame:button:1:target": process.env.__NEXT_PRIVATE_ORIGIN + "/claim/" + props.params.slug,
-      "og:title": tit,
-      "og:image": dataUri,
-    },
-  };
-};
-
-export default function Page(props: Props) {
-  return (
-    <Client slug={props.params.slug} />
-  );
-};
+}
